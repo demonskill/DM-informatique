c2b0012e295a3fb764f3c002ae75ce3484e177c4
demonskill <demonskill@localhost.localdomain>
2019-05-12 14:58:48 +0200

    amélioration de la mise en page,réduction de la taille du code

--- Dm3.py
+++ Dm3.py
@@ -11,7 +11,7 @@ def vsom(L_1,L_2) :
     for i in range(len(L_1)) :
         T.append(L_1[i] +L_2[i])
     return T
-    
+
 #Question I.3 b)
 def vdiff(L_1,L_2) :
     T=[]
@@ -25,10 +25,8 @@ def euler(f,t_min,t_max,n,z0,y0) :
     T=[]
     for i in range(n) :
         T.append(t_min + i*h)
-    z=z_0
-    y=y_0
-    Z=[z_0]
-    Y=[y_0]
+    z,y=z_0,y_0
+    Z,Y=[z_0],[y_0]
     for i in range(len(T)) :
         z,y= z + (T[i+1] - T[i])*f(y),y + (T[i+1] - T[i]) * z
         Z.append(z)
@@ -41,10 +39,8 @@ def verlet(f,t_min,t_max,n,z0,y0) :
     T=[]
     for i in range(n) :
         T.append(t_min + i*h)
-    z=z_0
-    y=y_0
-    Z=[z_0]
-    Y=[y_0]
+    z,y=z_0,y_0
+    Z,Y=[z_0],[y_0]
     for i in range(len(T)) :
         y= y + h*z + (h**2)/2*f(y_i)
         z= z+ h/2*(f(Y[-1])+f(y))
@@ -76,18 +72,15 @@ def euler_3d(f,t_min,t_max,n,z0,y0) :
     T=[]
     for i in range(n) :
         T.append(t_min + i*h)
-    z=z_0
-    y=y_0
-    Z=[z_0]
-    Y=[y_0]
+    z,y=z_0,y_0
+    Z,Y=[z_0],[y_0]
     for i in range(len(T)) :
         z,y= vsom(z, smul((T[i+1] - T[i]),f(y))),vsom(y, smul((T[i+1] - T[i]), z))
         Z.append(z)
         Y.append(y)
     return(Z,Y)
 def pos_suiv(m, pos, vit,h) :
-    pos2=[]
-    vit2=[]
+    pos2,vit2=[],[]
     def f(y) :
         return forceN(y,m,pos)
     for i in range(len(pos)) :
@@ -102,10 +95,8 @@ def verlet_3d(f,t_min,t_max,n,z0,y0) :
     T=[]
     for i in range(n) :
         T.append(t_min + i*h)
-    z=z_0
-    y=y_0
-    Z=[z_0]
-    Y=[y_0]
+    z,y=z_0,y_0
+    Z,Y=[z_0],[y_0]
     for i in range(len(T)) :
         y= vsom(vsom( y ,smul(h,z)),smul((h**2)/2,f(y)))
         z= vsom(z, smul(h/2,vsom(f(Y[-1]),f(y))))
